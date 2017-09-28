595a76a1a66598a50eb0fb1cf2d80a23a1c9a7ba
Jimmy <chen.jimmym@gmail.com>
2017-09-28 14:05:13 -0700

    working

---
+++ cc.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+    $('form').submit(function(){
+        var fn = $("input[type=text][name=firstname]").val();
+        var ln = $("input[type=text][name=lastname]").val();
+        var des = $("textarea[name=description]").val();
+        // alert(des);
+        $("#card").append('<div class="cardItem tog"><h3>' + fn + '</h3><h5>' + ln + '</h5><p>Click for description</p><div class="description tog"><p>'+des+'</p></div></div>');
+        $("#cardItem").append('<div class="cardItem description tog"><p>'+des+'</p></div>');
+        return false;
+    });
+
+    $(document).on('click', '.cardItem', function(){
+        // alert('you clicked a button!');
+        $(this).children().toggle(200);
+    });
+
+});
