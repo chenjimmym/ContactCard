4c0751c38d4905168d94406c2b70fa7f95797018
Jimmy <chen.jimmym@gmail.com>
2017-09-28 15:38:58 -0700

    done

--- cc.js
+++ cc.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
         var des = $("textarea[name=description]").val();
         // alert(des);
         $("#card").append('<div class="cardItem tog"><h3>' + fn + '</h3><h5>' + ln + '</h5><p>Click for description</p><div class="description tog"><p>'+des+'</p></div></div>');
-        $("#cardItem").append('<div class="cardItem description tog"><p>'+des+'</p></div>');
+        // $("#cardItem").append('<div class="cardItem description tog"><p>'+des+'</p></div>');
         return false;
     });
 
     $(document).on('click', '.cardItem', function(){
-        // alert('you clicked a button!');
+        // console.log($(this));
         $(this).children().toggle(200);
     });
 
